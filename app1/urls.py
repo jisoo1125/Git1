3044f3c5ad64aefbdc12b6ec10cfe03d41371598
zhaoxinyu <zxy15386848261@163.com>
2019-12-09 08:41:33 +0800

    赵鑫宇

--- app1/urls.py
+++ app1/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('tailong/', views.tailong, name='tailong'),
     path('xiaoyu/',views.xiaoyu,name='xiaoyu'),
     path('wxun/',views.wxun,name='wxun'),
+    path('zhaoxinyu/',views.zxy,name='zhaoxinyu'),
 ]
